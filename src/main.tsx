b0724112ff303b6a8a2854c59934d778e3cb025b
JAY DAVE <davejay1999@gmail.com>
2026-08-21 13:30:16 -0400

    fix: use explicit workout timer controller

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import outputs from './amplify_outputs.json';
-import JayFitApp from './app/JayFitApp';
+import JayFitTimerSafe from './app/JayFitTimerSafe';
 import AuthGate from './AuthGate';
 import './styles.css';
 import './additions.css';
@@ -11,7 +11,7 @@ Amplify.configure(outputs as any);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate>{({user,logout}) => <JayFitApp user={user} logout={logout}/>}</AuthGate>
+    <AuthGate>{({user,logout}) => <JayFitTimerSafe user={user} logout={logout}/>}</AuthGate>
   </React.StrictMode>
 );
 
