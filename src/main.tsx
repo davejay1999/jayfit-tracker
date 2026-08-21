76fcf1bcae96828ab8eb94dc7328ff6bfc28686a
JAY DAVE <davejay1999@gmail.com>
2026-08-21 01:51:52 -0400

    feat: configure Amplify auth and cloud data

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import outputs from './amplify_outputs.json';
+import App from './App';
+import AuthGate from './AuthGate';
+import './styles.css';
+
+Amplify.configure(outputs as any);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
-)
+  <React.StrictMode>
+    <AuthGate>{({user,logout}) => <App user={user} logout={logout}/>}</AuthGate>
+  </React.StrictMode>
+);
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(console.warn));
 }
